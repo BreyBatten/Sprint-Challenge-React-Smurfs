3e58d90c4ece4b31348ac6da703eb4b5b96ff682
BreyBatten <47111428+BreyBatten@users.noreply.github.com>
2019-06-14 23:09:54 -0400

    added axios.delete method on smurf.js

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import Axios from 'axios';
 
 const Smurf = props => {
+
+  const deleteSmurf = () => {
+    Axios.delete(`http://localhost:3333/smurfs/${props.id}`)
+    .then(res => console.log(res))
+    .catch(err => console.log(err));
+  }
   return (
     <div className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <button className="delete-btn" onClick={deleteSmurf}>Delete Smurf</button>
     </div>
   );
 };
